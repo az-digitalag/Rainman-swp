55939eb669f2fbeff504db5d08995595c95b140c
Jessica Guo <jessicaguo@email.arizona.edu>
2023-03-28 08:55:41 -0700

    Split app2.R into 3 separate scripts

---
+++ soildata_app/ui.R
@@ -0,0 +1,84 @@
+# UI
+
+# Define UI for application with three tabs
+shinyUI(navbarPage("RainManSR",
+                   tabPanel("Daily time series",
+                            titlePanel("Explore by treatment and season"),
+                            # Sidebar with drop down input for treatments, times, and date ranges
+                            sidebarLayout(
+                              sidebarPanel(
+                                # Select Summer treatment
+                                selectInput(inputId = "Summer",
+                                            label = "Select summer treatment",
+                                            choices = levels(Ts_daily$Summer)),
+                                # # Select Winter treatment
+                                selectInput(inputId = "Winter",
+                                            label = "Select winter treatment",
+                                            choices = unique(Ts_daily$Winter)),
+                                # Select Year
+                                selectInput(inputId = "Year",
+                                            label = "Select hydrological year", 
+                                            choices = unique(season$Year)), 
+                                # Select Season
+                                uiOutput("dyn_season"),
+                                # Select range of dates
+                                uiOutput("dyn_slider")
+                              ),
+                              # Show a size plot for selected species
+                              mainPanel(
+                                h4("Mean over plots by treatment"),
+                                fluidRow(plotOutput("seasonal_ts", width = "100%", height = "800px")),
+                                h5("For top two panels, error bars represent the range from the mean minimum to the mean maximum across plots of the same treatment. "),
+                                h5("For the bottom panel, error bars represent the daily range in varaiables. ")
+                              )
+                            )),
+                   tabPanel("Compare treatments",
+                            titlePanel("VWC by treatment type and year"),
+                            # Sidebar with drop down input for treatments, times, and date ranges
+                            sidebarLayout(
+                              sidebarPanel(
+                                # Select treatment
+                                selectInput(inputId = "Treatment",
+                                            label = "Select treatment type",
+                                            choices = c("Summer", "Winter")),
+                                # Select Year
+                                selectInput(inputId = "Year1",
+                                            label = "Select hydrological year", 
+                                            choices = unique(season$Year)),
+                                # Select range of dates
+                                uiOutput("dyn_slider1")
+                              ),
+                              # Show a size plot for selected species
+                              mainPanel(
+                                fluidRow(plotOutput("treatment_ts", width = "100%", height = "800px"))
+                              )
+                            )),
+                   tabPanel("Compare SWP and VWC",
+                            titlePanel("Relationships by treatment and year"),
+                            sidebarLayout(
+                              sidebarPanel(
+                                # Select Plot from House 3
+                                selectInput(inputId = "Summer2",
+                                            label = "Select summer treatment",
+                                            choices = unique(WPWC_daily$Summer)),
+                                # Select Year
+                                selectInput(inputId = "Year2",
+                                            label = "Select hydrological year", 
+                                            choices = unique(season$Year)), 
+                                # Select range of dates
+                                uiOutput("dyn_slider2")
+                              ),
+                              
+                              # Show a size plot for selected species
+                              mainPanel(
+                                h5('Click on point to obtain values. '),
+                                fluidRow(plotOutput("WPWC_scatter", 
+                                                    width = "100%", 
+                                                    height = "300px",
+                                                    click = clickOpts("plot_click"))),
+                                uiOutput("click_info"),
+                                fluidRow(plotOutput("WPWC_ts", width = "100%", height = "500px"))
+                              )
+                            ))
+)
+)
